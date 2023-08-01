8b908b59cceeea161b19316f6ca9aed9b062cde8
Bilal Khan <bilal.khan@qordata.com>
2023-08-01 17:19:36 +0500

    add login, registeration and authentication in server via tinydb

--- server/app.py
+++ server/app.py
@@ -1,124 +1,64 @@
-"""
-This script runs the application using a development server.
-It contains the definition of routes and views for the application.
-"""
-
-from flask import Flask, jsonify, request
-from flask_restful import Api, Resource
-from sqlalchemy.sql.expression import cast
-from flask_cors import CORS
-import sqlalchemy as sa
-import pandas as pd
-import json
+from flask import Flask, request
+from flask_restful import Resource, Api
+from tinydb import TinyDB, Query
+import bcrypt
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 
 app = Flask(__name__)
 api = Api(app)
-CORS(app)
 
-class getData(Resource):
-    def get(self):
-        db = request.args.get('db') 
-        table = request.args.get('table') 
-        server = request.args.get('server')
-        engine = sa.create_engine('mssql+pyodbc://'+server+'/'+db+'?driver=SQL+Server+Native+Client+11.0', echo=True)
+app.config['JWT_SECRET_KEY'] = 'this is a very strong secret key!'  # Change this!
+jwt = JWTManager(app)
+
+# Initialize the TinyDB database
+db = TinyDB('D:\Compliance Monitoring\Logs-Screen\server\db.json')
+
+# class Register for signing users 
+class Register(Resource):
+    def post(self):
+        username = request.json.get('username')
+        password = request.json.get('password')
+
+        User = Query()
+        user_exists = db.search(User.username == username)
+
+        if user_exists:
+            return {"message": "User already exists"}, 400
+        else:
+            # Hash the password
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            
+            # Store the username and hashed password in the database
+            db.insert({'username': username, 'password': hashed_password.decode('utf-8')})
+            return {"message": "User created successfully"}, 201    
+
+# class Login for logging in users
+class Login(Resource):
+    def post(self):
+        username = request.json.get('username')
+        password = request.json.get('password')
+
+        User = Query()
+        user = db.search(User.username == username)
         
-        with engine.connect() as connection:
-            metadata = sa.MetaData()
-            census = sa.Table(table, metadata, autoload=True, autoload_with=engine)
-            if table == "kri_details":
-                query = sa.select([census.c.ID, census.c.kri_name, census.c.template_agg_dimension, census.c.template_agg_column, census.c.Weightage, census.c.YTD, census.c.data_type, census.c.date_field, census.c.date_from, census.c.date_to, census.c.time_frame, census.c.is_active]) 
-            elif table == "kri_parameters":
-                query = sa.select([census.c.ID, census.c.kri_id, census.c.param_id, census.c.Param_value])
-            elif table == "RiskAlgoParameters":
-                query = sa.select([census.c.ID, census.c.RiskAlgoID, census.c.Param_name, census.c.Param_type, census.c.isMandatory])
-            elif table == "ValidationErrors":
-                query = sa.select([census.c.ID, census.c.Year, census.c.DataType, census.c.VersionColID, census.c.RuleID, census.c.TransactionID, census.c.LZRecordID])
-            elif table == "time_frames":
-                query = sa.select([census.c.id, census.c.timeSpan, cast(census.c.isActive, sa.Integer)])
-            elif table == "RiskyTransactions":
-                query = sa.select([census.c.ID, census.c.DataType, census.c.KRIID, census.c.RiskAlgoID, census.c.TemplateAggDimension, census.c.TemplateAggDimensionValue, census.c.TemplateAggColumn, census.c.TemplateAggColumnValue, census.c.RiskScore, census.c.WeightScore, census.c.DateFrom, census.c.DateTo])
-            elif table == "WarningTable":
-                query = sa.select([census.c.Id, census.c.KRIID, census.c.Message, census.c.Method, census.c.ErrorColumn, census.c.ErrorTable, census.c.LineNumber, census.c.IsCritical, census.c.Timestamp])
-            elif table == "lookback_configuration":
-                query = sa.select([census.c.id, census.c.kriid, census.c.kriname, census.c.lookback_period])
+        # Check the password against the hashed password in the database
+        if user:
+            if bcrypt.checkpw(password.encode('utf-8'), user[0]['password'].encode('utf-8')):
+                access_token = create_access_token(identity=username)
+                return {"message": "Login succeeded!", "access_token": access_token}, 200
             else:
-                return 404
-            ResultProxy = connection.execute(query)
-            ResultSet = ResultProxy.fetchall()
+                return {"message": "Invalid credentials"}, 401
+        else:
+            return {"message": "User does not exist"}, 400
 
-        df = pd.DataFrame(ResultSet)
-        if df.empty:
-            return 200
-        df.columns = ResultSet[0].keys()
-        retMap = df.to_json(orient='records')
-        return retMap
-
-    def post(self):
-        param = request.get_json()
-        db = param["db"]
-        server = param["server"]
-        queries = param["queries"]
-        engine = sa.create_engine('mssql+pyodbc://'+server+'/'+db+'?driver=SQL+Server+Native+Client+11.0', echo=True)
-        with engine.connect() as conn:
-            for query in queries:
-                try:
-                    conn.execute(query)
-                except:
-                    return 400
-
-        return 200
-
-
-class Databases(Resource):
+class Protected(Resource):
+    @jwt_required()
     def get(self):
-        #Database connection
-        server = request.args.get('server')
-        engine = sa.create_engine('mssql+pyodbc://'+server+'/master?driver=SQL+Server+Native+Client+11.0', echo=True)
-        query = "SELECT name FROM sys.databases WHERE name LIKE 'ComplianceMonitoring%';"
-        data = pd.read_sql(query, engine)
-        retMap = data['name'].to_json()
-        return retMap
+        return {"message": "You are authorized!"}, 200
 
-class JobSteps(Resource):
-    def get(self):
-        server = request.args.get('server')
-        job_name = request.args.get('job_name')
-        engine = sa.create_engine('mssql+pyodbc://'+server+'/msdb?driver=SQL+Server+Native+Client+11.0', echo=True)
-        try:
-            query = "EXEC sp_help_jobstep @job_name = '"+job_name+"';"
-            data = pd.read_sql(query, engine)
-            retMap = data['step_name'].to_json()
-        except:
-            return 400        
-        return retMap
+api.add_resource(Login, '/login')
+api.add_resource(Register, '/register')
+api.add_resource(Protected, '/protected')
 
-    def post(self):
-        param = request.get_json()
-        job_name = param["job_name"]
-        start_step = param["start_step"]
-        server = param["server"]
-        engine = sa.create_engine('mssql+pyodbc://'+server+'/ComplianceMonitoring?driver=SQL+Server+Native+Client+11.0', echo=True)
-        with engine.connect() as conn:
-            query = "EXEC job_execution @job_name = '"+job_name+"', @step_name = '"+start_step+"';"
-            result = pd.read_sql(query, engine)
-            retMap = result['ResultValue'].to_json()
-
-        return retMap
-
-api.add_resource(getData, '/getData')
-api.add_resource(Databases, '/getDatabaseName')
-api.add_resource(JobSteps, '/jobSteps')
-
-@app.route('/')
-def hello():
-    """Renders a sample page."""
-    return "Hello World!"
-
-#if __name__ == '__main__':
-    #import os
-    #HOST = os.environ.get('SERVER_HOST', 'localhost')
-    #try:
-    #    PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    #except ValueError:
-    #    PORT = 5555
-    #app.run(HOST, PORT)
+if __name__ == '__main__':
+    app.run(debug=True)
