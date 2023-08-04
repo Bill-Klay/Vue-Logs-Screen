c0f3855a4dda22021f3417c19d7b0d7fdf1a7d3a
Bilal Khan <bilal.khan@qordata.com>
2023-08-05 00:41:31 +0500

    login frontend complete

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,75 +1,50 @@
 <template>
-  <v-container class="fill-height">
-    <v-responsive class="align-center text-center fill-height">
-      <v-img height="300" src="@/assets/logo.svg" />
+  <div>
+    <v-text-field 
+        v-model="password"
+        :counter="8"
+        :rules="passwordRules"
+        label="Password"
+        :append-icon="value ? 'mdi-eye' : 'mdi-eye-off'"
+        @click:append="() => (value = !value)"
+        :type="value ? 'password' : 'text'"
+        required>
+    </v-text-field>
 
-      <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
-
-      <h1 class="text-h2 font-weight-bold">Vuetify</h1>
-
-      <div class="py-14" />
-
-      <v-row class="d-flex align-center justify-center">
-        <v-col cols="auto">
-          <v-btn
-            href="https://vuetifyjs.com/components/all/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
-            <v-icon
-              icon="mdi-view-dashboard"
-              size="large"
-              start
-            />
-
-            Components
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            color="primary"
-            href="https://vuetifyjs.com/introduction/why-vuetify/#feature-guides"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            target="_blank"
-            variant="flat"
-          >
-            <v-icon
-              icon="mdi-speedometer"
-              size="large"
-              start
-            />
-
-            Get Started
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            href="https://community.vuetifyjs.com/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
-            <v-icon
-              icon="mdi-account-group"
-              size="large"
-              start
-            />
-
-            Community
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-responsive>
-  </v-container>
+    <v-text-field 
+        v-model="email"
+        :rules="emailRules"
+        label="E-mail"
+        required>
+    </v-text-field>
+  </div>
 </template>
 
-<script setup>
-  //
+<script>
+    export default {
+      name: 'HelloWorld',
+      data() {
+        return {
+            password: '',
+            passwordRules: [
+                v => !!v || 'Password is required',
+                v => (v && v.length >= 8) || 'Password must be atleast 8 characters',
+            ],
+            email: '',
+            emailRules: [
+                v => !!v || 'E-mail is required',
+                v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+            ],
+            value: String
+        }
+      },
+      watch: {
+        password() {
+          this.$emit('changeStatus', this.password.length >= 8 && /.+@.+\..+/.test(this.email))
+        },
+        email() {
+          this.$emit('changeStatus', this.password.length >= 8 && /.+@.+\..+/.test(this.email))
+        }
+      }
+    }
 </script>
---
+++ src/components/Login.vue
@@ -0,0 +1,99 @@
+<template>
+    <v-container>
+        <v-row class="mt-5 mb-16">
+            <v-col></v-col>
+            <v-col>
+                <v-row>
+                    <v-col cols="4">
+                        <img src="..\assets\Pandas_Black.png" style="max-width: 100%; max-height: 100%; margin-top: 20px;"/>
+                    </v-col>
+                    <v-col cols="4">
+                        <img src="..\assets\qordata_logo.png" style="margin-top: 100%; position: relative; max-height: 27%;"/>
+                    </v-col>
+                </v-row>
+                <HelloWorld @changeStatus="loginButtonStatus"/>
+                <v-btn 
+                    :disabled="!validLogin" 
+                    color="primary" 
+                    title="With great power comes great responsibility">
+                    Login
+                </v-btn>
+                <v-dialog transition="dialog-top-transition" width="40%">
+                    <template v-slot:activator="{ props }">
+                    <a v-bind="props" style="margin-left: 5%; cursor: pointer; color: purple;">
+                        New to compliance monitoring?
+                    </a>
+                    </template>
+                    <template v-slot:default="{ isActive }">
+                        <v-card>
+                        <v-toolbar color="purple" title="Welcome to the team!"></v-toolbar>
+                        <v-card-text>
+                            <HelloWorld @changeStatus="signupButtonStatus"/>
+                        </v-card-text>
+                        <v-card-actions class="justify-end">
+                        <v-btn color="purple" @click="isActive.value = false" :disabled="!validSignup">
+                            Sign me up!
+                        </v-btn>
+                        <v-btn color="purple" @click="isActive.value = false">
+                            Maybe Later?
+                        </v-btn>
+                        </v-card-actions>
+                        </v-card>
+                    </template>
+                </v-dialog>
+            </v-col>
+            <v-col></v-col>
+        </v-row>
+        <v-row justify="center">
+            <v-col cols="8">
+                <div style="border: 1px solid grey; text-align: center;">
+                    Made with <v-icon color="error" icon="mdi-heart"></v-icon> by yours truly using
+                    <v-icon color="success" icon="mdi-vuejs"></v-icon> and
+                    <v-icon color="primary" icon="mdi-vuetify"></v-icon> and
+                    <v-icon color="warning" icon="mdi-language-python"></v-icon>
+                </div>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+    import HelloWorld from './HelloWorld.vue'
+    export default {
+        name: 'Login',
+        components: {
+            HelloWorld
+        },
+        data() {
+            return {
+                validLogin: false,
+                validSignup: false,
+                password: '',
+                passwordRules: [
+                    v => !!v || 'Password is required',
+                    v => (v && v.length >= 8) || 'Password must be atleast 8 characters',
+                ],
+                email: '',
+                emailRules: [
+                    v => !!v || 'E-mail is required',
+                    v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+                ],
+                value: String,
+            }
+        },
+        methods: {
+            loginButtonStatus(newStatus) {
+                this.validLogin = newStatus
+            },
+            signupButtonStatus(newStatus) {
+                this.validSignup = newStatus
+            }
+        }
+    }
+</script>
+
+<style>
+    div {
+        font-family: 'Victor Mono'
+    }
+</style>
