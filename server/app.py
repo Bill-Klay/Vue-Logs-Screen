8c09737c5ad02685e310dc660e962e1ba9b2d8d1
Bilal Khan <bilal.khan2998@gmail.com>
2023-10-20 12:17:15 +0500

    Update app.py adjust API key

--- server/app.py
+++ server/app.py
@@ -177,7 +177,7 @@ class PowerBI(Resource):
             print("Could not read file")
             return Response(status=400)
         try:
-            llm = OpenAI(api_token="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")
+            llm = OpenAI(api_token="your_key_here") # Change this!
             pandas_ai = PandasAI(llm)
             res = pandas_ai(df, prompt=args['prompt'])
             if isinstance(res, str) or isinstance(res, int):
