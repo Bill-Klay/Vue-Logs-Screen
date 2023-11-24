f2ae438792eef3a5ea2adaa68e384481e78b0877
Bill-Klay <bilal.khan2998@gmail.com>
2023-11-24 15:58:17 +0500

    update SSR to Hybrid rendering

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const host = "http://localhost"
 const port = 8081
 
 // We'll use the dist directory to serve the Vue App
 app.use(express.static('dist'));
+
+// All other GET requests not handled before will return our Vue app
+app.get('*', (req, res) => {
+ res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(port, () => {
  console.log(`App listening on ${host}:${port}`);
